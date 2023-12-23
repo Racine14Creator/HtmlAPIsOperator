6af8194a0416d5be4c3eab6592bc511759abd09a
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2023-12-23 11:56:09 +0200

    Dashboard icons

--- public/js/User/CreateUser.js
+++ public/js/User/CreateUser.js
@@ -2,6 +2,10 @@ const myForm = document.getElementById("myForm")
 
 myForm.onsubmit = (event) => {
     event.preventDefault()
-    const formData = new FormData()
-    console.log(formData)
+    const formData = new FormData(myForm)
+
+    fetch("http://localhost:7000/users", { method: "POST", body: formData })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
 }
