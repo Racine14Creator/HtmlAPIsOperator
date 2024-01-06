077db8883dedd5de13a22f3be85babca3d3f15b9
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-01-06 10:56:40 +0200

    CREATE user

--- public/js/User/CreateUser.js
+++ public/js/User/CreateUser.js
@@ -1,7 +1,23 @@
-const myForm = document.getElementById("myForm");
+const myForm = document.getElementById("myForm"),
+    error = document.getElementById("Error")
 
-myForm.addEventListener("submit", function (e) {
-    e.preventDefault()
-    const payload = new FormData(myForm)
-    console.log([...payload])
+myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const prePayload = new FormData(myForm)
+    const payload = new URLSearchParams(prePayload)
+
+    console.log(payload)
+    fetch("http://localhost:7000/users", {
+        method: "POST",
+        body: prePayload
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.message === 'success') {
+                window.location.href = 'users.html'
+            } else {
+                error.innerHTML = `<p class="alert-danger">${data.message}</p>`
+            }
+        })
+        .catch(error => console.log(error));
 })
