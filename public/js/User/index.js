f3bdec1c4187bd94194d6b6672fe9abf0ca3b76d
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2024-01-15 06:18:14 +0200

    profile

--- public/js/User/index.js
+++ public/js/User/index.js
@@ -1,1 +1,13 @@
 const urlAPI = "https://api-host-data.onrender.com"
+
+const button = document.getElementById("opendMenu"),
+    buttonx = document.getElementById("closeMenu")
+
+button.addEventListener("click", function () {
+    document.getElementById("sidebar").style.marginLeft = 0
+})
+
+buttonx.addEventListener("click", function () {
+    document.getElementById("sidebar").style.marginLeft = '-250px'
+})
+
